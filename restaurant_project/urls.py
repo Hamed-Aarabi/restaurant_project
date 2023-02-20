ab7060162615cfe2d030b5e34e7b1641f904fcc4
Hamed <108879210+Hamed-Aarabi@users.noreply.github.com>
2023-02-20 19:13:44 +0330

    before customize user and auth

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from . import settings, views
+from django.views.generic import  RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('home', views.HomeView.as_view(), name='home'),
+    path('', views.RedirectView.as_view(url='home'), name='go_to_home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
